05cdc5afe01fbce9f8aa1b74829b3ccfd05c2e74
Adam Kimball <adamchasekimball@gmail.com>
2018-02-15 16:10:58 -0700

    gameboard formatting

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,20 +5,22 @@ import Login from './Login';
 import Register from './Register';
 import Flash from './Flash';
 import Home from './Home';
+import Gameboard from './Gameboard';
 import ProtectedRoute from './ProtectedRoute';
 import AuthRoute from './AuthRoute';
 import FetchUser from './FetchUser';
 import { Switch, Route } from 'react-router-dom';
+import { Divider } from 'semantic-ui-react';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <NavBar />
         <Flash />
+        <Divider hidden />
         <FetchUser>
           <Switch>
-            <Route exact path='/' component={Home} />
+            <Route exact path='/' component={Gameboard} />
             <AuthRoute exact path='/login' component={Login} />
             <AuthRoute exact path='/register' component={Register} />
             <Route component={NoMatch} />
--- client/src/components/Gameboard.js
+++ client/src/components/Gameboard.js
@@ -5,19 +5,19 @@ class Gameboard extends React.Component {
   render() {
     return(
       <Grid columns={3}>
-        <Grid.Column textAlign='center' width={3} style={{backgroundColor: 'green'}}>
+        <Grid.Column textAlign='center' width={3} style={{backgroundColor: 'green', height: '80vh'}}>
           <Header>Player Two</Header>
         </Grid.Column>
-        <Grid.Column textAlign='center' width={10}>
-          <Grid.Row textAlign='center' style={{backgroundColor: 'red'}}>
+        <Grid.Column textAlign='center' width={10} style={{height: '80vh'}}>
+          <Grid.Row textAlign='center' style={{backgroundColor: 'red', height: '20vh'}}>
             <Header>Player Three</Header>
           </Grid.Row>
-          <Grid.Row></Grid.Row>
-          <Grid.Row textAlign='center' style={{backgroundColor: 'yellow'}}>
+          <Grid.Row style={{height: '40vh'}}></Grid.Row>
+          <Grid.Row textAlign='center' style={{backgroundColor: 'yellow', height: '20vh'}}>
             <Header>Player One</Header>
           </Grid.Row>
         </Grid.Column>
-        <Grid.Column textAlign='center' width={3} style={{backgroundColor: 'blue'}}>
+        <Grid.Column textAlign='center' width={3} style={{backgroundColor: 'blue', height: '80vh'}}>
           <Header>Player Four</Header>
         </Grid.Column>
       </Grid>
